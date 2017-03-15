16da2a217e276026b62e6ae6232cc41435a9a217
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2017-03-15 18:42:39 -0400

    fix bugs

--- src/proquint.erl
+++ src/proquint.erl
@@ -41,7 +41,7 @@ decode(Binary) when is_binary(Binary) ->
 do_encode(Data) ->
 	[ <<(encode_word(A))/binary >> || << A:16/bits >> <= Data ].
 
-do_decode(Data) -> << <<decode_byte(Char)/binary>> || <<Char:8/integer> <= Data>>>.
+do_decode(Data) -> << <<(decode_byte(Char))/binary>> || <<Char:8/integer>> <= Data >>.
 
 
 encode_word(<< A:4/integer, B:2/integer, C:4/integer, D:2/integer, E:4/integer >>) ->
@@ -56,24 +56,24 @@ encode_word(<< A:4/integer, B:2/integer, C:4/integer, D:2/integer, E:4/integer >
 encode(N, vowel)     -> ?LOOKUP_VOWEL(N);
 encode(N, consonant) -> ?LOOKUP_CONSONANT(N).
 
-decode_byte($a)-> << 0:2/bits >>;
-decode_byte($i)-> << 1:2/bits >>;
-decode_byte($o)-> << 2:2/bits >>;
-decode_byte($u)-> << 3:2/bits >>;
+decode_byte($a)-> << 0:2/integer >>;
+decode_byte($i)-> << 1:2/integer >>;
+decode_byte($o)-> << 2:2/integer >>;
+decode_byte($u)-> << 3:2/integer >>;
 
-decode_byte($b)-> << 0:4/bits >>;
-decode_byte($d)-> << 1:4/bits >>;
-decode_byte($f)-> << 2:4/bits >>;
-decode_byte($g)-> << 3:4/bits >>;
-decode_byte($h)-> << 4:4/bits >>;
-decode_byte($j)-> << 5:4/bits >>;
-decode_byte($k)-> << 6:4/bits >>;
-decode_byte($l)-> << 7:4/bits >>;
-decode_byte($m)-> << 8:4/bits >>;
-decode_byte($n)-> << 9:4/bits >>;
-decode_byte($p)-> << 10:4/bits >>;
-decode_byte($r)-> << 11:4/bits >>;
-decode_byte($s)-> << 12:4/bits >>;
-decode_byte($t)-> << 13:4/bits >>;
-decode_byte($v)-> << 14:4/bits >>;
-decode_byte($z)-> << 15:4/bits >>.
+decode_byte($b)-> << 0:4/integer >>;
+decode_byte($d)-> << 1:4/integer >>;
+decode_byte($f)-> << 2:4/integer >>;
+decode_byte($g)-> << 3:4/integer >>;
+decode_byte($h)-> << 4:4/integer >>;
+decode_byte($j)-> << 5:4/integer >>;
+decode_byte($k)-> << 6:4/integer >>;
+decode_byte($l)-> << 7:4/integer >>;
+decode_byte($m)-> << 8:4/integer >>;
+decode_byte($n)-> << 9:4/integer >>;
+decode_byte($p)-> << 10:4/integer >>;
+decode_byte($r)-> << 11:4/integer >>;
+decode_byte($s)-> << 12:4/integer >>;
+decode_byte($t)-> << 13:4/integer >>;
+decode_byte($v)-> << 14:4/integer >>;
+decode_byte($z)-> << 15:4/integer >>.
